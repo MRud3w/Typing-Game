6aafcaf80bf16f433db5a13b53182b20545872bb
MRud3w <128928104+MRud3w@users.noreply.github.com>
2024-03-13 18:30:16 +0100

    added typing event listener

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
 const wordsBox = document.querySelector('.words');
+const gameArea = document.querySelector('.game');
 const randomWords = 'plausible dreamless make thermodynamics raid composure bicycle none run shoal involution storm dolce imaging cutaway member bootleg Nycticorax landmark regulation'.split(' ');
+const wordsCount = randomWords.length;
+
+const createRandomWord = () => {
+    const randomIndex = Math.floor(Math.random() * randomWords.length);
+    return randomWords[randomIndex];
+}
+
+const addClass = (el, val) => {
+    el.className += ' ' + val;
+}
+
+const removeClass = (el, val) => {
+    el.className = el.className.replace(val,'')
+}
+
+const formatWord = (word) => {
+    let formattedWord = '';
+    for (let i = 0; i < word.length; i++) {
+        formattedWord += `<span class="letter">${word[i]}</span>`;
+    }
+    return `<div class="word">${formattedWord}</div>`;
+}
 
 const startGame = () => {
     wordsBox.innerHTML = '';
-
-    for (let i = 0; i < 10; i++) {
-        const randomIndex = Math.floor(Math.random() * randomWords.length);
-        const randomWord = randomWords[randomIndex];
-        const wordElement = document.createElement('div');
-        wordElement.textContent = randomWord;
-        wordsBox.appendChild(wordElement);
+    for (let i = 0; i < 100; i++) {
+        wordsBox.innerHTML += formatWord(createRandomWord());
     }
+    addClass(document.querySelector('.word'), 'current');
+    addClass(document.querySelector('.letter'), 'current');
 }
+
+gameArea.addEventListener('keydown', (e) => {
+    const key = e.key;
+    const currentLetter = document.querySelector('.letter.current');
+    const expected = currentLetter.innerHTML;
+    console.log({key, expected});
+});
+
 
 startGame();
 
